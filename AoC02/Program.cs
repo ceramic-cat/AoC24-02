2f1e630eaaf1889d2659dd7e5e7477e38fc1989b
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2024-12-04 16:48:27 +0100

    Create list of arrays of numbers out of the input.

---
+++ AoC02/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second day of code");
+
+            var inputData = File.ReadLines("input-data.txt");
+            Console.WriteLine(inputData.Count());
+
+            List<int[]> reports = new List<int[]>();
+
+            foreach (var line in inputData)
+            {
+                string[] substrings = line.Split(' ');
+                int[] numberArray = new int[substrings.Length];
+                for (int i = 0; i < substrings.Length; i++)
+                {
+                    int number = int.Parse(substrings[i]);
+                    numberArray[i] = number;
+                }
+                reports.Add(numberArray);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
